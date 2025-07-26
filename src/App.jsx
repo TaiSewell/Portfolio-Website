131c6734ac0d9b0b30a78f0d127a8b310dbba868
TaiSewell <Tai.sewell1@gmail.com>
2025-07-25 19:37:38 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-400">Hi, I'm Tai Sewell</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
