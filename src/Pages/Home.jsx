a2dcdc93bac50fa3b95d221e9451aa291b2835ff
TaiSewell <Tai.sewell1@gmail.com>
2025-08-14 15:18:08 -0500

    Moved Profile and notebook import assets to public now

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,8 +8,8 @@
 import { useState, useEffect, useRef } from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import profilePic from '../assets/Profile.jpg';
-import IntroLoader from "../Components/IntroLoader";
+import profilePic from './assets/Profile.jpg';
+import IntroLoader from "./Components/IntroLoader";
 
 /************************************************
  * Function: Home()
